ea171d98fc0b70d652ff8009be6bfc9cc1ca3579
snufk1nsp1pe <rs.naceri@gmail.com>
2025-08-21 16:35:22 +0100

    fixed MiniArticle for better ux

--- app/ui/MiniArticle.tsx
+++ app/ui/MiniArticle.tsx
@@ -1,32 +1,32 @@
 import Link from 'next/link'
 // import ReactMarkdown from "react-markdown";
 import { Post } from '@/lib/types'
-export default function MiniArticle({ post }: {post: Post}) {
+export default function MiniArticle({ post }: { post: Post }) {
   const { bg, text, border, meta } = post.palette
 
   return (
-      <article
-        className={` relative ${border} ${bg} border-2 border-dashed rounded-2xl md:p-5 p-3 leading-6 ${text} ${post.colSpan} ${post.rowSpan} break-inside-avoid mb-4`}>
+    <article
+      className={` relative ${border} ${bg} border-2 border-dashed rounded-2xl md:p-5 p-3 leading-6 ${text} ${post.colSpan} ${post.rowSpan} break-inside-avoid mb-4`}>
+      <Link href={`/notebook/${post.slug}`}>
         <header className='flex justify-between md:pb-5 pb-3 gap-2'>
-          <Link href={`/notebook/${post.slug}`}>
-            <h2 className='font-semibold md:text-2xl text-xl'>{post.title}</h2>
-          </Link>
+          <h2 className='font-semibold md:text-2xl text-xl'>{post.title}</h2>
           <span className={`${meta} font-light md:text-sm text-xs`}>
             <time>{post.date}</time>
           </span>
         </header>
         {/* <ReactMarkdown>{post.tagline}</ReactMarkdown> */}
-        <span className='md:text-lg  '>{post.tagline}</span>
-        <div className='md:mt-4 mt-2'>
-          {post.tags?.map((tag, i) => (
-            <Link
-              href={`/notebook/tags/${tag}`}
-              key={i}
-              className={`${meta} font-semibold md:text-[16px] text-sm`}>
-              {`#${tag} `}{' '}
-            </Link>
-          ))}
-        </div>
-      </article>
+        <div className='md:text-lg  '>{post.tagline}</div>
+      </Link>
+      <div className='md:mt-4 mt-2'>
+        {post.tags?.map((tag, i) => (
+          <Link
+            href={`/notebook/tags/${tag}`}
+            key={i}
+            className={`${meta} font-semibold md:text-[16px] text-sm`}>
+            {`#${tag} `}{' '}
+          </Link>
+        ))}
+      </div>
+    </article>
   )
 }
